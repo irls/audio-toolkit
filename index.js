32f3f887670c3ba3a265e2e457b212e0ff975850
David Hunt <git@dnotes.net>
2017-06-05 15:18:02 -0700

    updated index.js to call ffmpeg with task-agnostic omnipotence

--- index.js
+++ index.js
@@ -2,31 +2,45 @@
 
 var fs = require('fs')
 const exec = require('child_process').exec
-			
 
 prepEnvironment = function(){
 	return new Promise(function(resolve, reject) {
-		let docker = exec('"eval $(docker-machine env default"')
+		let docker = exec('"eval $(docker-machine env default)"')
 		docker.stdout.on('close', code => {
 			resolve($(code))
 		})
 		docker.stdout.on('exit', code => {
 			resolve($(exit))
 		})
-		docker.stdout.on('error', err =>{
+		docker.stdout.on('error', err => {
 			reject(err)
 		})
 	}	
 }
 
-module.exports.mergeFiles = function(data_folder_path){
+/**
+ * Task list 
+ *
+ * AF: Audio File for an entire audiobook
+ * BLAF: Block Level Audio File for a single section
+ *
+ * - import: converts disparate audio files to .flac and joins into AF
+ * - combineAll: joins BLAFs into AF, e.g. to prepare for mastering
+ * - insert: inserts an audio selection into a BLAF
+ * - delete: deletes an audio selection from a BLAF
+ * - replace: replaces an audio selection from a BLAF
+ * - splitBlock: splits a BLAF into two
+ * - joinBlocks: joins two BLAFs into one
+ * - export: convert to .ogg or .mp3 (TODO: choose) for publishing
+ */
+module.exports.ffmpeg = function(data_folder_path, task_name, ...args){
 	return new Promise(function(resolve, reject) {
 		fs.ensureDir(data_folder_path, function(err){
 			if(err) reject(err)
 			else {
 				let prep = prepEnvironment()
 				prep.then(function(result){
-					let docker = exec("'docker' run --rm  --entrypoint='bash' -v $(pwd)/test:/data ffmpeg_merge merge_files.sh")
+					let docker = exec("'docker' run --rm -d -v " + data_folder_path + ":/data /app/" + task_name + ".sh " + join(' ', args))
 					docker.stdout.on('close', code => {
 						resolve($(code))
 					})
