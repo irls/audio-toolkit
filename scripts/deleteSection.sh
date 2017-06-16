a8f8298b038cbc716618a9797a5e0815d91ac1c6
Chad <chadananda@gmail.com>
2017-06-16 06:20:33 -0700

    commented deleteSection.sh script

--- scripts/deleteSection.sh
+++ scripts/deleteSection.sh
@@ -7,4 +7,13 @@
 # 	ft='mp3'
 # fi
 
+# // deletes section, resolves to destFile
+# deleteSection(srcFile, fromPos, toPos, [destFile])
+# processAudio(tmpDir, 'deleteSection', fileName(tmpSrc), fileName(tmpDest), fromPos, toPos)
+
+# tmpDir: contains source audio file
+# fileName(tmpSrc): source audio file, stored in the tmpDir
+# fileName(tmpDest): expected target file name, to be placed in tmpDir
+# fromPos, toPos: start and end positions expressed in ms
+
 ffmpeg -f concat -safe 0 -i <(for f in /data/*.mp3; do echo "file '$f'"; done) -c copy /data/output.wav
