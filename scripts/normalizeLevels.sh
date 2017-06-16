e85fb6ce098a4f1b5dfb4aa7f096e637fd50303e
Chad <chadananda@gmail.com>
2017-06-16 06:36:55 -0700

    commented normalizeLevels.sh and normalizeSilence.sh scripts

--- scripts/normalizeLevels.sh
+++ scripts/normalizeLevels.sh
@@ -7,4 +7,12 @@
 # 	ft='mp3'
 # fi
 
+# // normalize volume levels
+# normalizeLevels(srcFile, [destfile], [options])
+# processAudio(tmpDir, 'normalizeLevels', fileName(tmpSrc), fileName(tmpDest))
+
+# tmpDir: contains source audio file
+# fileName(tmpSrc): source audio file, stored in the tmpDir
+# fileName(tmpDest): output audio file, also stored n the tmpDir
+
 ffmpeg -f concat -safe 0 -i <(for f in /data/*.mp3; do echo "file '$f'"; done) -c copy /data/output.wav
