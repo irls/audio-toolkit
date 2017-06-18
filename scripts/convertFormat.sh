e4bc7f0be59f25adacbef61f828f8dd970d05a9d
David Hunt <git@dnotes.net>
2017-06-17 21:30:07 -0700

    fixed convertFormat script

--- scripts/convertFormat.sh
+++ scripts/convertFormat.sh
@@ -2,15 +2,13 @@
 #
 # // Converts all files in the /data/source/ folder to a specified format.
 # // $1 toFormat: The destination format.
-# // $2 inputDir: subfolder containing source files, should be "input/"
-# // $3 outputDir: subfolder with converted files, should be "output/"
 
 # for all files in inputDir
-echo 'convertFormat.sh, $1, $2'
+echo 'convertFormat.sh, $1'
 
-for i in /data/$2*.*;
+for i in /data/*.*;
   # get the src file name
-  do name=`echo $i | cut -d'.' -f1`;
+  do filepath=`echo $i | cut -d'.' -f1`;
   # convert and place output in outputDir with new extension
-  ffmpeg -i "$i" "$3${name}.$1";
+  ffmpeg -i "$i" "${filepath}.$1";
 done
