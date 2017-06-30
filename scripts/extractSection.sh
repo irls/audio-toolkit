0c71146c0ba38387e1d4ebed349ede4b4d0ea1cc
Chad <chadananda@gmail.com>
2017-06-30 09:54:53 +0200

    added extractSection script

---
+++ scripts/extractSection.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+
+# Delete section. Split, split then merge
+
+# $1 inputFile: The file name of the source audio, with extension.
+# $2 outputFile: The filename for the audio before the split position.
+# $3 fromPos: The position at which the source file should be split.
+# $4 toPos: The position at which the source file should be split.
+
+# ext="${1##*.}"
+
+# extract the selection to outputFile
+ffmpeg -i "/data/$1" -acodec copy -t $3 -ss $4 "/data/$2"
+
+
+
+# mark this task complete
+touch "/data/taskcomplete.marker"
