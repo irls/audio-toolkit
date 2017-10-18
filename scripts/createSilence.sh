21071826942128843a7e9027adbc2087d597e48b
ylis <ylis@isddesign.com>
2017-10-18 11:03:59 +0300

    New version of insert silence script

--- scripts/createSilence.sh
+++ scripts/createSilence.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 # Generates silence file in "/data/" folder
-# $1 (length): the length of necessarysilence
-# $1 (destFile): the silence file name
+# $1 (length): the length of necessary silence
+# $2 (destFile): the silence file name
 
 ffmpeg -f lavfi -i aevalsrc=0:0::duration=$1 -ab 320k /data/$2
 
